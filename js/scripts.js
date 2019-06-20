be1cf6d90ed9b87279e653e62adc8d17e70376a4
Devin Cooley <dcooley1350@gmail.com>
2019-06-20 13:28:08 -0700

    add first coded spec.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
-
-
+var PingPong = function(usernumber) {
+  return false;
+}
 
 
 
 
 $(document).ready(function() {
-  event.preventDefault();
-  var userNumber = parseInt($("input#number-entered").val());
-  var pingPongGame = PingPong(userNumber);
-  $("#pingPongGame").text(pingPongGame)
-})
+  $("form#user-input").submit(function(event) {
+    event.preventDefault();
+    var userNumber = parseInt($("input#number-entered").val());
+    var pingPongGame = PingPong(userNumber);
+    $("#pingPongGame").append("<li>"+pingPongGame+"</li>");
+  });
+});
