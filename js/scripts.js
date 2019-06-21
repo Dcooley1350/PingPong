208ec60a9ace3d2749adac556a8a0be29d31e9e1
Devin Cooley <dcooley1350@gmail.com>
2019-06-20 18:43:12 -0700

    fix appending bug where previous entries did not go away upong new submit.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
-var arrayNumbers = []
-var PingPong = function(userNumber) {
+
+var PingPong = function(userNumber,arrayNumbers) {
   if ((userNumber < 0) || (userNumber % 1 != 0)){
+    console.log('array numbers set to false');
     arrayNumbers = false
   } else {
     for (var index =0; index < userNumber; index += 1) {
@@ -16,14 +17,19 @@ var PingPong = function(userNumber) {
 
 $(document).ready(function() {
   $("form#user-input").submit(function(event) {
+    var arrayNumbers = []
+    $("#pingPongGame").empty()
     event.preventDefault();
     var userNumber = parseInt($("input#number-entered").val());
-    PingPong(userNumber);
-
+    PingPong(userNumber,arrayNumbers);
+console.log("ine 225")
+    if (arrayNumbers=== true) {
+      console.log(arrayNumbers)
+      $("#pingPongGame").append("<li>"+"Please enter a postitive integer!"+"<li>")
+    }
     arrayNumbers.forEach(function(arrayNumber) {
-      if (!arrayNumber) {
-        $("#pingPongGame").append("<li>"+"Please enter a postitive integer!"+"<li>")
-      } else if (arrayNumber % 15 === 0) {
+
+      if (arrayNumber % 15 === 0) {
         $("#pingPongGame").append("<li>"+"Ping Pong"+"</li>")
       } else if (arrayNumber % 5 === 0) {
         $("#pingPongGame").append("<li>"+"Pong"+"</li>")
