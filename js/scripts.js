2e51781afd012fdfd735d9c5396a8eb171c91bc2
Devin Cooley <dcooley1350@gmail.com>
2019-06-20 17:09:55 -0700

    write script for 'forEach loop to return numbers to user.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
-var PingPong = function(usernumber) {
-  if ((usernumber < 0) || (userNumber % 1 != 0)){
-    return false
-  } 
+var arrayNumbers = []
+var PingPong = function(userNumber) {
+  if ((userNumber < 0) || (userNumber % 1 != 0)){
+    arrayNumbers = false
+  } else {
+    for (var index =0; index < userNumber; index += 1) {
+      arrayNumbers.push(index+1)
+    }
+  }
 }
+
+
 
 
 
@@ -11,7 +18,19 @@ $(document).ready(function() {
   $("form#user-input").submit(function(event) {
     event.preventDefault();
     var userNumber = parseInt($("input#number-entered").val());
-    var pingPongGame = PingPong(userNumber);
-    $("#pingPongGame").append("<li>"+pingPongGame+"</li>");
+    PingPong(userNumber);
+
+    arrayNumbers.forEach(function(arrayNumber) {
+      if (!arrayNumber) {
+        $("#pingPongGame").append("<li>"+"Please enter a postitive integer!"+"<li>")
+      } else if (arrayNumber % 3 === 0) {
+        $("#pingPongGame").append("<li>"+"Ping"+"</li>")
+      } else if (arrayNumber % 5 === 0) {
+        $("#pingPongGame").append("<li>"+"Pong"+"</li>")
+      } else if (arrayNumber % 15 === 0) {
+        $("#pingPongGame").append("<li>"+"Ping Pong"+"</li>")
+      } else
+        $("#pingPongGame").append("<li>"+arrayNumber+"</li>");
+    })
   });
 });
